ab04724a1c919ce7a19de126838d8140591f0554
chloecatherinepotter <chloepotter446@gmail.com>
2024-04-22 11:51:18 -0500

    New changes added

--- src/main/java/CaesarCipher/java/CaesarCipher.java
+++ src/main/java/CaesarCipher/java/CaesarCipher.java
@@ -5,9 +5,10 @@ package CaesarCipher.java;
 import java.util.Scanner;
 
 public class CaesarCipher {
-
+    //This is the string that lists the letters of the alphabet.
     static String alphabet = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
 
+    //This encodes the text entered by the user.
     private static String encoding(String pText, int Key) {
         pText = pText.toLowerCase();
         String cText ="";
@@ -20,6 +21,7 @@ public class CaesarCipher {
         return cText;
     }
 
+    //This decodes the string after it is encoded.
     public static String decoding(String cText, int key) {
         cText = cText.toLowerCase();
         String pText ="";
@@ -35,12 +37,15 @@ public class CaesarCipher {
         return pText;
     }
 
+    //This prints the text so the user can enter the Plain Text and the Key.
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        //This asks for the Plain Text.
         System.out.print("Enter Plain Text >> ");
         String plain = scan.nextLine();
 
+        //This asks for the key.
         System.out.print("Enter Key >> ");
         int Key = scan.nextInt();
 
